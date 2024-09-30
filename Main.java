42d085e0fe76f6b2ed7d52161bad90fe00148717
spielgrund <138134622+spielgrund@users.noreply.github.com>
2024-09-30 13:47:16 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,10 @@
 package Enaira;
-
 import java.util.Arrays;
+
+//Die Raumsonde Vojager befindet sich am Rande des Sonnensystems. Um trotzdem Nachrichten in guter Qualität zur Erde schicken zu können,
+//werden die Nachrichten mehrfach hintereinander gesendet, damit man Fehler in der Übertragung ausgleichen kann. Baue diese Funktion in Java nach.
+//Die Nachrichten sollen erst zu einem bestimmten Prozentsatz zufällig verrauscht werden. Danach werden mehrere verrauschte Nachrichten gesammelt
+//und wieder entrauscht. Viel Erfolg!
 
 public class Main {
     public static void main(String[] args) {
